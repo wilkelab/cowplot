6e0583552c4a4a85eb319cc0527563f4a980a375
Claus Wilke <wilke@austin.utexas.edu>
2014-10-05 17:38:55 -0500

    Adding stub for a vignette.

--- R/cowplot.R
+++ R/cowplot.R
@@ -1,5 +1,16 @@
-theme_cow <- function() theme_classic(base_size=14)
+#' Default cowplot theme
+#'
+#' After loading the cowplot package, this theme will be the default
+#' for all graphs made with ggplot2.
+#' 
+#' @param base_size The overall font size. Default is 14.
+#' @return The theme.
+#' @examples
+#' qplot(1:10, 1:10^2) + theme_cow(base_size=16)
+theme_cow <- function(base_size=14) ggplot2::theme_classic(base_size)
 
-ggplot <- function(...) ggplot(...) + theme_cow()
 
-qplot <- function(...) qplot(...) + theme_cow()
+.onAttach <- function(libname, pkgname) {
+    # switch the default theme to theme_cow
+    ggplot2::theme_set(theme_cow())
+}
