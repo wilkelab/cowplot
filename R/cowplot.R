8c80505b38d45b89036edfbb835e80dd15a7a1da
Claus Wilke <wilke@austin.utexas.edu>
2014-10-06 18:28:03 -0500

    Things are starting to come together.

--- R/cowplot.R
+++ R/cowplot.R
@@ -13,10 +13,11 @@ NULL
 # *************************************************
 
 .onAttach <- function(libname, pkgname) {
-  # switch the default theme to theme_cow
-  ggplot2::theme_set(theme_cow())
+  # switch the default theme to theme_cowplot
+  ggplot2::theme_set(theme_cowplot())
 }
 
+.pt <- 1 / 0.352777778 # from ggplot2, file aaa-constants.r
 
 # *************************************************
 #                     Themes
@@ -27,25 +28,34 @@ NULL
 #' After loading the cowplot package, this theme will be the default
 #' for all graphs made with ggplot2.
 #'
-#' @param base_size The overall font size. Default is 14.
-#' @param base_family The default family.
+#' @param font_size The overall font size. Default is 13.
+#' @param font_family The default font family.
 #' @return The theme.
 #' @examples
-#' qplot(1:10, 1:10^2) + theme_cow(base_size = 16)
-theme_cow <- function(base_size = 14, base_family = "") {
-  # Starts with theme_grey and then modify some parts
-  theme_grey(base_size = base_size, base_family = base_family) %+replace%
+#' qplot(1:10, 1:10^2) + theme_cow(font_size = 15)
+theme_cowplot <- function(font_size = 13, font_family = "", line_size = .5) {
+  # Start with default theme_grey and then modify some parts
+  theme_grey(base_size = font_size, base_family = font_family) %+replace%
     theme(
       rect              = element_rect(fill = "transparent", colour = NA, color = NA, size = 0, linetype = 0),
-      axis.text         = element_text(size = base_size),
-      axis.ticks        = element_line(colour = "black"),
-      axis.line         = element_line(colour = "black"),
+      text              = element_text(family = font_family, face = "plain", colour = "black",
+                                       size = font_size, hjust = 0.5, vjust = 0.5, angle = 0, lineheight = .9),
+      axis.text         = element_text(colour = "black", size = font_size),
+      axis.title.x      = element_text(vjust = 0),
+      axis.title        = element_text(face = "bold"),
+      axis.ticks        = element_line(colour = "black", size = line_size),
+      axis.line         = element_line(colour = "black", size = line_size),
       legend.key        = element_blank(),
+      legend.margin     = grid::unit(0.1, "cm"),
+      legend.key.size   = grid::unit(1, "lines"),
+  #    legend.position   = c(-0.03, 1.05),
+  #    legend.justification = c("left", "top"),
       panel.background  = element_blank(),
-      plot.background   = element_blank(),
       panel.border      = element_blank(),
       panel.grid.major  = element_blank(),
       panel.grid.minor  = element_blank(),
+      plot.background   = element_blank(),
+      plot.title        = element_text(face = "bold", size = font_size),
       strip.background  = element_rect(fill = "grey80", colour = "grey50", size = 0)
     )
 }
@@ -70,10 +80,11 @@ theme_nothing <- function(base_size = 12, base_family = ""){
       # to debug, uncomment next line
       #plot.background   = element_rect(colour = "blue", fill = "cyan"),
       panel.background  = element_blank(),
-      axis.ticks.margin = unit(0, "lines"),
-      axis.ticks.length = unit(0, "lines"),
-      panel.margin      = unit(c(0, 0, 0, 0), "lines"),
-      plot.margin       = unit(c(0, 0, 0, 0), "lines")
+      axis.ticks.margin = grid::unit(0, "lines"),
+      axis.ticks.length = grid::unit(0, "lines"),
+      legend.position   = "none",
+      panel.margin      = grid::unit(c(0, 0, 0, 0), "lines"),
+      plot.margin       = grid::unit(c(0, 0, 0, 0), "lines")
     )
 }
 
@@ -92,9 +103,10 @@ ggsave <- function(...){
 #'
 #' This function is just a thin wrapper around ggsave.
 save_plot <- function(filename, plot, cols = 1, rows = 1,
-                      base_width = 5, base_height = 3.5, dpi = 300,
-                      units = "in", useDingbats = FALSE){
-  ggplot2::ggsave(filename, plot, width = base_width*cols, height = base_height*rows, units, useDingbats, dpi)
+                      base_width = 5, base_height = 4, dpi = 300,
+                      useDingbats = FALSE){
+  ggplot2::ggsave(filename = filename, plot = plot, width = base_width*cols, height = base_height*rows,
+                  useDingbats = useDingbats)
 }
 
 
@@ -112,7 +124,7 @@ save_plot <- function(filename, plot, cols = 1, rows = 1,
 #' @param y vector of y coordinates
 #' @param ... style parameters, such as 'colour', 'alpha', size', etc.
 draw_line <- function(x, y, ...){
-  geom_line(data = data.frame(x, y),
+  geom_path(data = data.frame(x, y),
             aes(x = x, y = y),
             ...)
 }
@@ -122,6 +134,9 @@ draw_line <- function(x, y, ...){
 #' This is a convenience function. It's just a thin wrapper around geom_text. It can take either an
 #' individual piece of text to be drawn or a vector of separate text labels, with associated coordinates.
 #'
+#' Note that font sizes get scaled by a factor of 2.85, so sizes given here agree with font sizes used in
+#' the theme. This is different from geom_text in ggplot2.
+#'
 #' By default, the x and y coordinates specify the center of the text box. Set hjust = 0, vjust = 0 to specify
 #' the lower left corner, and other values of hjust and vjust for any other relative location you want to
 #' specify.
@@ -129,9 +144,10 @@ draw_line <- function(x, y, ...){
 #' @param x vector of x coordinates
 #' @param y vector of y coordinates
 #' @param ... style parameters, such as 'colour', 'alpha', 'angle', 'size', etc.
-draw_text <- function(text, x, y, ...){
+draw_text <- function(text, x = 0.5, y = 0.5, size = 13, ...){
   geom_text(data = data.frame(text, x, y),
             aes(label = text, x = x, y = y),
+            size = size / .pt, # scale font size to match size in theme definition
             ...)
 }
 
@@ -139,14 +155,14 @@ draw_text <- function(text, x, y, ...){
 #'
 #' This function adds a plot label to the upper left corner of a graph. It takes all the same parameters
 #' as draw_text(), but has defaults that make it convenient to label graphs.
-draw_plot_label <- function(label, x=0, y=1, hjust = -0.3, vjust = 1.3, size = 14, fontface = 'bold', ...){
+draw_plot_label <- function(label, x=0, y=1, hjust = -0.5, vjust = 1.5, size = 16, fontface = 'bold', ...){
   draw_text(text = label, x = x, y = y, hjust = hjust, vjust = vjust, size = size, fontface = fontface, ...)
 }
 
 
 #' Draw a (sub)plot.
 #'
-#' Place a plot somewhere onto the drawing canvas. By default, coordinates run from
+#' Places a plot somewhere onto the drawing canvas. By default, coordinates run from
 #' 0 to 1, and the point (0, 0) is in the lower left corner of the canvas.
 #' @param plot The plot to place
 #' @param x x location of the lower left corner of the plot
@@ -178,27 +194,12 @@ ggdraw <- function(plot = NULL){
 # and then place the original graph and annotations using annotation_custom()
 
 test <- function(plot){
-  d <- data.frame(x=0:1, y=0:1) # dummy data
-  p <- ggplot(d, aes(x=x, y=y)) + # empty plot
-    scale_x_continuous(limits = c(0, 1), expand = c(0, 0)) +
-    scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) +
-    theme_nothing() + # with empty theme
-    labs(x=NULL, y=NULL) # and absolutely no axes
-  plot.grob <- grid::grobTree(ggplot2::ggplotGrob(plot))
-  text.grob <- grid::grobTree(grid::textGrob("this is a test", x = 0.45,  y = 0.5, rot = 45,
-                                          gp=grid::gpar(col = "red", alpha = .4, fontsize = 120)))
-  p + annotation_custom(text.grob) + annotation_custom(plot.grob) + draw_line(c(1, .5), c(0, .5), colour='green', size=3)
+  p <- qplot(x=1:10, y=(1:10)^2) #+ theme_gray()
+  p_ann <- ggdraw(p) +
+    draw_plot_label("A") +
+    draw_text("Draft", size = 90, colour = 'red', angle = 45, alpha = .3)
+  print(p_ann)
+  save_plot("test.pdf", p_ann)
 }
-
-
-
-# Add a watermark to a plot
-# http://www.r-bloggers.com/adding-watermarks-to-plots/
-watermark <- function(label="This is a Draft!"){
-  my_grob = grid::grobTree(grid::textGrob(label, x = 0.5,  y = 0.5, rot = 45,
-                                          gp=grid::gpar(col = "red", alpha = .4, fontsize = 150)))
-  annotation_custom(my_grob)
-}
-
 
 
