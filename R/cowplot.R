70bc988ada0923c0f8c3742d9493d23113e66572
Claus Wilke <wilke@austin.utexas.edu>
2014-10-06 13:04:46 -0500

    Lots of progress. Some stuff is working already.

--- R/cowplot.R
+++ R/cowplot.R
@@ -7,6 +7,21 @@
 NULL
 
 
+
+# *************************************************
+#                     Setup
+# *************************************************
+
+.onAttach <- function(libname, pkgname) {
+  # switch the default theme to theme_cow
+  ggplot2::theme_set(theme_cow())
+}
+
+
+# *************************************************
+#                     Themes
+# *************************************************
+
 #' Create the default cowplot theme
 #'
 #' After loading the cowplot package, this theme will be the default
@@ -62,30 +77,102 @@ theme_nothing <- function(base_size = 12, base_family = ""){
     )
 }
 
+
+
+# *************************************************
+#                     Output
+# *************************************************
+
 # never, ever again worry about misrendered dingbats
 ggsave <- function(...){
   ggplot2::ggsave(..., useDingbats = FALSE)
 }
 
-#' Add a watermark to a plot
-# http://www.r-bloggers.com/adding-watermarks-to-plots/
-watermark <- function(label="Draft"){
-  my_grob = grid::grobTree(grid::textGrob(label, x = 0.5,  y = 0.5, rot = 45,
-                            gp=grid::gpar(col = "red", alpha = .4, fontsize = 56)))
-  annotation_custom(my_grob)
+#' Alternative to ggsave.
+#'
+#' This function is just a thin wrapper around ggsave.
+save_plot <- function(filename, plot, cols = 1, rows = 1,
+                      base_width = 5, base_height = 3.5, dpi = 300,
+                      units = "in", useDingbats = FALSE){
+  ggplot2::ggsave(filename, plot, width = base_width*cols, height = base_height*rows, units, useDingbats, dpi)
 }
 
-#' Draw an annotation line.
+
+
+# *************************************************
+#                     Drawing code
+# *************************************************
+
+
+#' Draw a line.
+#'
+#' This is a convenience function. It's just a thin wrapper around geom_line.
 #'
 #' @param x vector of x coordinates
 #' @param y vector of y coordinates
 #' @param ... style parameters, such as 'colour', 'alpha', size', etc.
-ann_line <- function(x, y, ...){
+draw_line <- function(x, y, ...){
   geom_line(data = data.frame(x, y),
             aes(x = x, y = y),
             ...)
 }
 
+#' Draw text.
+#'
+#' This is a convenience function. It's just a thin wrapper around geom_text. It can take either an
+#' individual piece of text to be drawn or a vector of separate text labels, with associated coordinates.
+#'
+#' By default, the x and y coordinates specify the center of the text box. Set hjust = 0, vjust = 0 to specify
+#' the lower left corner, and other values of hjust and vjust for any other relative location you want to
+#' specify.
+#' @param text character or expression vector specifying the text to be written.
+#' @param x vector of x coordinates
+#' @param y vector of y coordinates
+#' @param ... style parameters, such as 'colour', 'alpha', 'angle', 'size', etc.
+draw_text <- function(text, x, y, ...){
+  geom_text(data = data.frame(text, x, y),
+            aes(label = text, x = x, y = y),
+            ...)
+}
+
+#' Draw plot label
+#'
+#' This function adds a plot label to the upper left corner of a graph. It takes all the same parameters
+#' as draw_text(), but has defaults that make it convenient to label graphs.
+draw_plot_label <- function(label, x=0, y=1, hjust = -0.3, vjust = 1.3, size = 14, fontface = 'bold', ...){
+  draw_text(text = label, x = x, y = y, hjust = hjust, vjust = vjust, size = size, fontface = fontface, ...)
+}
+
+
+#' Draw a (sub)plot.
+#'
+#' Place a plot somewhere onto the drawing canvas. By default, coordinates run from
+#' 0 to 1, and the point (0, 0) is in the lower left corner of the canvas.
+#' @param plot The plot to place
+#' @param x x location of the lower left corner of the plot
+#' @param y y location of the lower left corner of the plot
+#' @param width width of the plot
+#' @param height height of the plot
+draw_plot <- function(plot, x = 0, y = 0, width = 1, height = 1){
+  plot.grob <- grid::grobTree(ggplot2::ggplotGrob(plot))
+  annotation_custom(plot.grob, xmin = x, xmax = x+width, ymin = y, ymax = y+height)
+}
+
+#' Set up a drawing layer on top of a ggplot
+ggdraw <- function(plot = NULL){
+  d <- data.frame(x=0:1, y=0:1) # dummy data
+  p <- ggplot(d, aes(x=x, y=y)) + # empty plot
+    scale_x_continuous(limits = c(0, 1), expand = c(0, 0)) +
+    scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) +
+    theme_nothing() + # with empty theme
+    labs(x=NULL, y=NULL) # and absolutely no axes
+
+  if (!is.null(plot)){
+    plot.grob <- grid::grobTree(ggplot2::ggplotGrob(plot))
+    p <- p + annotation_custom(plot.grob)
+  }
+  p # return ggplot drawing layer
+}
 
 # one method that will definitely work is to create a new graph that only holds an empty plotting area
 # and then place the original graph and annotations using annotation_custom()
@@ -100,10 +187,18 @@ test <- function(plot){
   plot.grob <- grid::grobTree(ggplot2::ggplotGrob(plot))
   text.grob <- grid::grobTree(grid::textGrob("this is a test", x = 0.45,  y = 0.5, rot = 45,
                                           gp=grid::gpar(col = "red", alpha = .4, fontsize = 120)))
-  p + annotation_custom(text.grob) + annotation_custom(plot.grob) + ann_line(c(1, .5), c(0, .5), colour='green', size=3)
+  p + annotation_custom(text.grob) + annotation_custom(plot.grob) + draw_line(c(1, .5), c(0, .5), colour='green', size=3)
 }
 
-.onAttach <- function(libname, pkgname) {
-    # switch the default theme to theme_cow
-    ggplot2::theme_set(theme_cow())
+
+
+# Add a watermark to a plot
+# http://www.r-bloggers.com/adding-watermarks-to-plots/
+watermark <- function(label="This is a Draft!"){
+  my_grob = grid::grobTree(grid::textGrob(label, x = 0.5,  y = 0.5, rot = 45,
+                                          gp=grid::gpar(col = "red", alpha = .4, fontsize = 150)))
+  annotation_custom(my_grob)
 }
+
+
+
