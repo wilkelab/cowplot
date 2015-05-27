48fe50e0ba385703e4208a891159b59974ed5263
Claus Wilke <wilke@austin.utexas.edu>
2015-05-27 00:13:19 -0500

    fix ggsave copy in cowplot

--- R/cowplot.R
+++ R/cowplot.R
@@ -152,9 +152,96 @@ panel_border <- function(colour = 'gray80', size = 0.5, linetype = 1, remove = F
 # *************************************************
 
 # never, ever again worry about misrendered dingbats
-# not currently exported because it is broken.
-ggsave <- function(...){
-  ggplot2::ggsave(..., useDingbats = FALSE)
+# this code is copied directly from ggplot2. ugly, but
+# the only way to implement this properly.
+ggsave <- function(filename = NULL, plot = ggplot2::last_plot(),
+                   device = default_device(filename), path = NULL, scale = 1,
+                   width = par("din")[1], height = par("din")[2], units = c("in", "cm", "mm"),
+                   dpi = 300, limitsize = TRUE, ...) {
+
+  if (is.null(filename)) stop("filename is required")
+  if (!inherits(plot, "ggplot")) stop("plot should be a ggplot2 plot")
+
+  eps <- ps <- function(..., width, height)
+    grDevices::postscript(..., width=width, height=height, onefile=FALSE,
+                          horizontal = FALSE, paper = "special")
+  tex <- function(..., width, height)
+    grDevices::pictex(..., width=width, height=height)
+  pdf <- function(..., version="1.4"){
+    if ("useDingbats" %in% names(list(...)))
+      grDevices::pdf(..., version=version)
+    else
+      grDevices::pdf(..., useDingbats = FALSE, version=version)
+  }
+  svg <- function(...)
+    grDevices::svg(...)
+  wmf <- function(..., width, height)
+    grDevices::win.metafile(..., width=width, height=height)
+  emf <- function(..., width, height)
+    grDevices::win.metafile(..., width=width, height=height)
+
+  png <- function(..., width, height)
+    grDevices::png(...,  width=width, height=height, res = dpi, units = "in")
+  jpg <- jpeg <- function(..., width, height)
+    grDevices::jpeg(..., width=width, height=height, res = dpi, units = "in")
+  bmp <- function(..., width, height)
+    grDevices::bmp(...,  width=width, height=height, res = dpi, units = "in")
+  tiff <- function(..., width, height)
+    grDevices::tiff(..., width=width, height=height, res = dpi, units = "in")
+
+  default_device <- function(filename) {
+    pieces <- strsplit(filename, "\\.")[[1]]
+    ext <- tolower(pieces[length(pieces)])
+    match.fun(ext)
+  }
+
+  units <- match.arg(units)
+  convert_to_inches <- function(x, units) {
+    x <- switch(units,
+                `in` = x,
+                cm = x / 2.54,
+                mm = x / 2.54 /10
+    )
+  }
+
+  convert_from_inches <- function(x, units) {
+    x <- switch(units,
+                `in` = x,
+                cm = x * 2.54,
+                mm = x * 2.54 * 10
+    )
+  }
+
+  # dimensions need to be in inches for all graphic devices
+  # convert width and height into inches when they are specified
+  if (!missing(width)) {
+    width <- convert_to_inches(width, units)
+  }
+  if (!missing(height)) {
+    height <- convert_to_inches(height, units)
+  }
+  # if either width or height is not specified, display an information message
+  # units are those specified by the user
+  if (missing(width) || missing(height)) {
+    message("Saving ", prettyNum(convert_from_inches(width * scale, units), digits=3), " x ", prettyNum(convert_from_inches(height * scale, units), digits=3), " ", units, " image")
+  }
+
+  width <- width * scale
+  height <- height * scale
+
+  if (limitsize && (width >= 50 || height >= 50)) {
+    stop("Dimensions exceed 50 inches (height and width are specified in inches/cm/mm, not pixels).",
+         " If you are sure you want these dimensions, use 'limitsize=FALSE'.")
+  }
+
+  if (!is.null(path)) {
+    filename <- file.path(path, filename)
+  }
+  device(file=filename, width=width, height=height, ...)
+  on.exit(capture.output(dev.off()))
+  print(plot)
+
+  invisible()
 }
 
 #' Alternative to ggsave.
