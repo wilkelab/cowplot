c3164a94a3d7f4be5c9afce0fcb46698d3c52293
Claus Wilke <wilke@austin.utexas.edu>
2015-06-09 19:27:15 -0500

    compiled vignette as well

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -3,13 +3,13 @@ require(ggplot2)
 
 ## ----message=FALSE-------------------------------------------------------
 ggplot(mpg, aes(x = cty, y = hwy, colour = factor(cyl))) + 
-   geom_point(size=2.5) + theme_gray()
+   geom_point(size = 2.5)
 
 ## ----echo=FALSE, message=FALSE-------------------------------------------
 require(cowplot)
 theme_set(theme_cowplot(font_size=12)) # default fontsize doesn't work well for online viewing
 plot.mpg <- ggplot(mpg, aes(x = cty, y = hwy, colour = factor(cyl))) + 
-  geom_point(size=2.5)
+  geom_point(size = 2.5)
 plot.mpg
 
 ## ----eval=FALSE----------------------------------------------------------
@@ -24,6 +24,10 @@ plot.mpg
 ## ----message=FALSE-------------------------------------------------------
 plot.mpg + background_grid(major = "xy", minor = "none")
 
+## ----eval=FALSE----------------------------------------------------------
+#  plot.mpg + theme_gray() # create plot with default ggplot2 theme
+#  theme_set(theme_gray()) # switch to default ggplot2 theme for good
+
 ## ----message=FALSE-------------------------------------------------------
 plot.mpg <- ggplot(mpg, aes(x = cty, y = hwy, colour = factor(cyl))) + 
   geom_point(size=2.5)
@@ -33,13 +37,16 @@ plot.diamonds <- ggplot(diamonds, aes(clarity, fill = cut)) + geom_bar() +
 plot.diamonds
 
 ## ----message=FALSE, fig.width=7, fig.height=2.5--------------------------
-plot_grid(plot.mpg, plot.diamonds, labels=c("A", "B"))
+plot_grid(plot.mpg, plot.diamonds, labels = c("A", "B"))
+
+## ----message=FALSE, fig.width=7, fig.height=2.5--------------------------
+plot_grid(plot.mpg, plot.diamonds, labels = c("A", "B"), align = "h")
 
 ## ----message=FALSE, fig.width=7, fig.height=5----------------------------
-plot_grid(plot.mpg, NULL, NULL, plot.diamonds, labels=c("A", "B", "C", "D"), ncol = 2)
+plot_grid(plot.mpg, NULL, NULL, plot.diamonds, labels = c("A", "B", "C", "D"), ncol = 2)
 
-## ----message=FALSE, fig.width=10, fig.height=2.5-------------------------
-plot_grid(plot.mpg, plot.diamonds, plot.mpg, labels=c("A", "B", "C"), nrow = 1)
+## ----message=FALSE, fig.width=4, fig.height=5----------------------------
+plot_grid(plot.mpg, plot.diamonds, labels = c("A", "B"), nrow = 2, align = "v")
 
 ## ----eval=FALSE----------------------------------------------------------
 #  plot2by2 <- plot_grid(plot.mpg, NULL, NULL, plot.diamonds,
@@ -86,21 +93,22 @@ plot.iris <- ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
   geom_point() + facet_grid(. ~ Species) + stat_smooth(method = "lm") +
   background_grid(major = 'y', minor = "none") + # add thin horizontal lines 
   panel_border() # and a border around each panel
-# plot.mpt and plot.diamonds were defined earlier
+# plot.mpg and plot.diamonds were defined earlier
 ggdraw() +
   draw_plot(plot.iris, 0, .5, 1, .5) +
   draw_plot(plot.mpg, 0, 0, .5, .5) +
   draw_plot(plot.diamonds, .5, 0, .5, .5) +
   draw_plot_label(c("A", "B", "C"), c(0, 0, 0.5), c(1, 0.5, 0.5), size = 15)
 
-## ----message=FALSE, fig.width=7, fig.height=5----------------------------
-ggdraw() +
-  #geom_rect(data = boxes, aes(xmin = x, xmax = x + .15, ymin = y, ymax = y + .15),
-  #          colour = "gray60", fill = "red", alpha=.03) +
-  geom_path(data = spiral, aes(x = x, y = y, colour = t), size = 6, alpha = .4) +
-  draw_plot(plot.diamonds, -.05, -.1, .55, .55) +
-  draw_plot(plot.diamonds, .65, .4, .5, .5) +
-  draw_plot(plot.mpg, .3, .3, .4, .4) +
-  draw_plot(plot.iris, 0, .7, .7, .35 ) +
-  draw_plot(plot.iris, .45, .0, .6, .3 )
+## ----eval=FALSE, echo=FALSE, message=FALSE, fig.width=7, fig.height=5----
+#  # Of course, we can also go crazy:
+#  ggdraw() +
+#    #geom_rect(data = boxes, aes(xmin = x, xmax = x + .15, ymin = y, ymax = y + .15),
+#    #          colour = "gray60", fill = "red", alpha=.03) +
+#    geom_path(data = spiral, aes(x = x, y = y, colour = t), size = 6, alpha = .4) +
+#    draw_plot(plot.diamonds, -.05, -.1, .55, .55) +
+#    draw_plot(plot.diamonds, .65, .4, .5, .5) +
+#    draw_plot(plot.mpg, .3, .3, .4, .4) +
+#    draw_plot(plot.iris, 0, .7, .7, .35 ) +
+#    draw_plot(plot.iris, .45, .0, .6, .3 )
 
