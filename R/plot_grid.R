f7e3edfffdb08c569e6094f80c139ba0d18dd7e5
Claus Wilke <wilke@austin.utexas.edu>
2017-08-03 00:35:13 -0400

    fix plot_grid documentation

--- R/plot_grid.R
+++ R/plot_grid.R
@@ -236,14 +236,16 @@ align_plots <- function(..., plotlist = NULL, align = c("none", "h", "v", "hv"),
 #' @param label_fontfamily (optional) Font family of the plot labels. If not provided, is taken from the current theme.
 #' @param label_fontface (optional) Font face of the plot labels. Default is "bold".
 #' @param label_colour (optional) Color of the plot labels. If not provided, is taken from the current theme.
-#' @param label_x (optional) Vector of x positions for plot labels, relative to each subplot.
+#' @param label_x (optional) Single value or vector of x positions for plot labels, relative to each subplot.
 #'   Defaults to 0 for all labels. (Each label is placed all the way to the left of each plot.)
-#' @param label_y (optional) Vector of y positions for plot labels, relative to each subplot.
+#' @param label_y (optional) Single value or vector of y positions for plot labels, relative to each subplot.
 #'   Defaults to 1 for all labels. (Each label is placed all the way to the top of each plot.)
 #' @param hjust Adjusts the horizontal position of each label. More negative values move the label further
-#'              to the right on the plot canvas. Default is -0.5.
+#'   to the right on the plot canvas. Can be a single value (applied to all labels) or a vector of values
+#'   (one for each label). Default is -0.5.
 #' @param vjust Adjusts the vertical position of each label. More positive values move the label further
-#'              down on the plot canvas. Default is 1.5.
+#'   down on the plot canvas. Can be a single value (applied to all labels) or a vector of values
+#'   (one for each label). Default is 1.5.
 #' @param scale Individual number or vector of numbers greater than 0. Enables you to scale the size of all or
 #'   select plots. Usually it's preferable to set margins instead of using `scale`, but `scale` can
 #'   sometimes be more powerful.
