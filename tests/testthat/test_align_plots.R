8e1e1bc3dec50421a7d9b7f4b9b7244d2878a4a7
Claus Wilke <wilke@austin.utexas.edu>
2017-08-10 20:31:28 -0500

    more tests

--- tests/testthat/test_align_plots.R
+++ tests/testthat/test_align_plots.R
@@ -18,3 +18,15 @@ test_that("basic alignments, h, v, hv", {
   expect_equal(plots[[1]]$widths, plots[[2]]$widths)
   expect_equal(plots[[1]]$heights, plots[[2]]$heights)
 })
+
+
+test_that("complex alignments, h, v, hv", {
+  p1 <- qplot(1:10, 1:10)
+  p2 <- ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + geom_point() + facet_wrap(~Species)
+
+  # these plots cannot be aligned unless axes are specified
+  expect_warning(align_plots(p1, p2, align = "v"))
+
+  plots <- align_plots(p1, p2, align = "v", axis = "l")
+  expect_equal(plots[[1]]$widths[1:3], plots[[2]]$widths[1:3])
+})
