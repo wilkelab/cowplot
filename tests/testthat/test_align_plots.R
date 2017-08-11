322b28b8abebd33f314f044d2d842c5951ce88bb
Claus Wilke <wilke@austin.utexas.edu>
2017-08-10 23:33:42 -0500

    better unit tests

--- tests/testthat/test_align_plots.R
+++ tests/testthat/test_align_plots.R
@@ -29,4 +29,26 @@ test_that("complex alignments, h, v, hv", {
 
   plots <- align_plots(p1, p2, align = "v", axis = "l")
   expect_equal(plots[[1]]$widths[1:3], plots[[2]]$widths[1:3])
+
+  p3 <- ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + geom_point() + facet_wrap(~Species, ncol=1)
+
+  # these plots cannot be aligned unless axes are specified
+  expect_warning(align_plots(p1, p3, align = "h"))
+
+  plots <- align_plots(p1, p3, align = "h", axis = "bt")
+  expect_equal(plots[[1]]$heights[7:10], plots[[2]]$heights[18:21])
+
+  # these units are only equal after we've added everything up
+  expect_equal(grid::convertUnit(plots[[1]]$heights[1] +
+                                 plots[[1]]$heights[2] +
+                                 plots[[1]]$heights[3] +
+                                 plots[[1]]$heights[4] +
+                                 plots[[1]]$heights[5] +
+                                 plots[[1]]$heights[6], "cm"),
+               grid::convertUnit(plots[[2]]$heights[1] +
+                                 plots[[2]]$heights[2] +
+                                 plots[[2]]$heights[3] +
+                                 plots[[2]]$heights[4] +
+                                 plots[[2]]$heights[5] +
+                                 plots[[2]]$heights[6], "cm"))
 })
