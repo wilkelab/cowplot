bb819a1211a7a43e5762af3acf6c62b8dfd9eb88
Claus Wilke <wilke@austin.utexas.edu>
2018-03-19 12:54:54 -0500

    improved comments, safer opening and closing of null device

--- R/plot_to_gtable.R
+++ R/plot_to_gtable.R
@@ -26,7 +26,7 @@ plot_to_gtable <- function(plot){
       # we convert the captured plot or output plot into a grob
       # to be safe, we have to save and restore the current graphics device
       cur_dev <- grDevices::dev.cur()
-      tree <- grid::grid.grabExpr(gridGraphics::grid.echo(plot))
+      tree <- grid::grid.grabExpr(gridGraphics::grid.echo(plot)) # capture plot
       grDevices::dev.set(cur_dev)
 
       u <- grid::unit(1, "null")
@@ -37,21 +37,24 @@ plot_to_gtable <- function(plot){
     }
   }
   else if (methods::is(plot, "ggplot")){
-    ## ggplotGrob must open a device and when a multiple page capable device (e.g. PDF) is open it will
-    ## save a blank page. To avoid this blank page, we open a NULL pdf device that will *absorb* the
-    ## blank plot. However, as part of this procedure, the wrong device can end up being the current one,
-    ## so to be absolutely sure, we also save the previous device and then restore it.
+    # Convert ggplot plot to grob
+    #
+    # To be safe this works as expected, we have to do some graphics-device gymnastics.
+    # We need to save and restore the current graphics device, and we also need to open
+    # a null device. If we don't do this, things may go wrong, in particular in R Studio
+    # or shiny, such as plots popping up in the wrong location or spurious empty plots
+    # appearing in knitr. Also, depending on which null device we choose, non-standard
+    # fonts may or may not work. Different null devices work best in different environments,
+    # that's why the null device is configurable. (`pdf(NULL)` is the most robust but
+    # can't handle all fonts, `png()` works well on OS X but creates spurious output files,
+    # `Cairo(type = "raster")` works well on Windows but font-handling is broken on OS X.)
 
-    # save currently active device
-    cur_dev <- grDevices::dev.cur()
-    # open NULL device
-    null_dev_env$current()
-    # convert plot to grob
-    plot <- ggplot2::ggplotGrob(plot)
-    # close null device
-    grDevices::dev.off()
-    # restore previously active device
-    grDevices::dev.set(cur_dev)
+    cur_dev <- grDevices::dev.cur()    # store current device
+    null_dev_env$current()             # open null device
+    null_dev <- grDevices::dev.cur()   # store null device
+    plot <- ggplot2::ggplotGrob(plot)  # convert plot to grob
+    grDevices::dev.off(null_dev)       # close null device
+    grDevices::dev.set(cur_dev)        # restore previously active device
 
     plot
   }
