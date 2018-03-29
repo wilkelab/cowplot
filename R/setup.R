3339ab6488f1666f17cea417677c960d31c259e9
Claus Wilke <wilke@austin.utexas.edu>
2018-03-29 16:21:15 -0500

    updated news

--- R/setup.R
+++ R/setup.R
@@ -5,6 +5,8 @@
 .onAttach <- function(libname, pkgname) {
   # switch the default theme to theme_cowplot
   # ggplot2::theme_set(theme_cowplot())
+  message("\n\n********************************************************")
   message("Note: cowplot does not change the default theme anymore.")
-  message("To recover the previous behavior, execute: theme_set(theme_cowplot())")
+  message("To recover the previous behavior, execute:\n  theme_set(theme_cowplot())")
+  message("********************************************************\n")
 }
