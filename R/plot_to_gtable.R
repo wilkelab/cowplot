688e3c6b9d8d155247549aca86cefe3aeea2c640
Claus Wilke <wilke@austin.utexas.edu>
2018-03-30 08:50:19 -0500

    support ggassembles

--- R/plot_to_gtable.R
+++ R/plot_to_gtable.R
@@ -141,6 +141,40 @@ as_grob.ggplot <- function(plot, device = NULL) {
 }
 
 #' @export
+as_grob.ggassemble <- function(plot, device = NULL) {
+  if (!requireNamespace("patchwork", quietly = TRUE)){
+    warning("Package `patchwork` is required to handle object of class ggassemble. Substituting empty plot.", call. = FALSE)
+    return(grid::nullGrob())
+  }
+
+  # Convert patchwork ggassemble to grob
+  #
+  # To be safe this works as expected, we have to do some graphics-device gymnastics.
+  # We need to save and restore the current graphics device, and we also need to open
+  # a null device. If we don't do this, things may go wrong, in particular in R Studio
+  # or shiny, such as plots popping up in the wrong location or spurious empty plots
+  # appearing in knitr. Also, depending on which null device we choose, non-standard
+  # fonts may or may not work. Different null devices work best in different environments,
+  # that's why the null device is configurable. (`pdf(NULL)` is the most robust but
+  # can't handle all fonts, `png()` works well on OS X but creates spurious output files,
+  # `Cairo(type = "raster")` works well on Windows but font-handling is broken on OS X.)
+
+  if (is.null(device)) {
+    device <- null_dev_env$current
+  }
+
+  cur_dev <- grDevices::dev.cur()   # store current device
+  device(width = 6, height = 6)     # open null device
+  null_dev <- grDevices::dev.cur()  # store null device
+
+  # make sure we always clean up properly, even if something causes an error
+  on.exit({grDevices::dev.off(null_dev); grDevices::dev.set(cur_dev)})
+
+  patchwork::patchworkGrob(plot)    # convert plot to grob
+}
+
+
+#' @export
 as_grob.default <- function(plot, device = NULL) {
   warning("Cannot convert object of class ", class(plot), "into a grob.")
 }
