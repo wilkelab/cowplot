2f792705907c5be85ec1df1800abb813d9929822
Claus Wilke <wilke@austin.utexas.edu>
2018-05-05 18:57:47 -0500

    add vdiffr helper function

---
+++ tests/testthat/helper-vdiffr.R
@@ -0,0 +1,21 @@
+enable_vdiffr <- TRUE
+
+expect_doppelganger <- function(title, fig,
+                                path = NULL,
+                                ...,
+                                user_fonts = NULL,
+                                verbose = FALSE) {
+  if (!enable_vdiffr) {
+    # expectation here is that figure can be built but we don't
+    # check how it looks
+    expect_error(regexp = NA, ggplot2::ggplot_build(fig))
+    return(invisible(NULL))
+  }
+
+  vdiffr::expect_doppelganger(title, fig,
+                              path = path,
+                              ...,
+                              user_fonts = user_fonts,
+                              verbose = verbose
+  )
+}
