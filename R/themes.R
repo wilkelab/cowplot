aa7a209ec83cca2683820102dc6803d7fad14749
Claus Wilke <wilke@austin.utexas.edu>
2018-05-05 21:04:49 -0500

    synch theme with ggplot2

--- R/themes.R
+++ R/themes.R
@@ -19,7 +19,7 @@
 #' @return The theme.
 #' @examples
 #' library(ggplot2)
-#' qplot(1:10, (1:10)^2) + theme_cowplot(font_size = 15)
+#' qplot(1:10, (1:10)^2) + theme_cowplot(font_size = 12)
 #' @importFrom ggplot2 margin
 #' @importFrom grid unit
 #' @export
@@ -28,52 +28,108 @@ theme_cowplot <- function(font_size = 14, font_family = "", line_size = .5,
   half_line <- font_size / 2
   small_size <- rel_small * font_size
 
+  # work off of theme_grey just in case some new theme element comes along
   theme_grey(base_size = font_size, base_family = font_family) %+replace%
     theme(
-      rect              = element_rect(fill = "transparent", colour = NA, color = NA, size = 0, linetype = 0),
+      line              = element_line(colour = "black", size = line_size, linetype = 1, lineend = "butt"),
+      rect              = element_rect(fill = NA, colour = NA, size = line_size, linetype = 0),
       text              = element_text(family = font_family, face = "plain", colour = "black",
                                        size = font_size, hjust = 0.5, vjust = 0.5, angle = 0, lineheight = .9,
                                        margin = margin(), debug = FALSE),
-      axis.text         = element_text(colour = "black", size = small_size),
-      #axis.title        = element_text(face = "bold"),
-      axis.text.x       = element_text(margin = margin(t = small_size / 4), vjust = 1),
-      axis.text.y       = element_text(margin = margin(r = small_size / 4), hjust = 1),
-      axis.title.x      = element_text(margin = margin(t = small_size / 2)),
-      axis.title.y      = element_text(angle = 90,
-                                       margin = margin(r = small_size / 2)),
-      axis.ticks        = element_line(colour = "black", size = line_size),
+
       axis.line         = element_line(colour = "black", size = line_size, lineend = "square"),
-      # the following two lines are not needed for ggplot2 2.2.0 or later
-      axis.line.x       = element_line(colour = "black", size = line_size, lineend = "square"),
-      axis.line.y       = element_line(colour = "black", size = line_size, lineend = "square"),
+      axis.line.x       = NULL,
+      axis.line.y       = NULL,
+      axis.text         = element_text(colour = "black", size = small_size),
+      axis.text.x       = element_text(margin = margin(t = small_size / 4), vjust = 1),
+      axis.text.x.top   = element_text(margin = margin(b = small_size / 4), vjust = 0),
+      axis.text.y       = element_text(margin = margin(r = small_size / 4), hjust = 1),
+      axis.text.y.right = element_text(margin = margin(l = small_size / 4), hjust = 0),
+      axis.ticks        = element_line(colour = "black", size = line_size),
+      axis.ticks.length = unit(half_line / 2, "pt"),
+      axis.title.x      = element_text(
+                            margin = margin(t = half_line / 2),
+                            vjust = 1
+                          ),
+      axis.title.x.top  = element_text(
+                            margin = margin(b = half_line / 2),
+                            vjust = 0
+                          ),
+      axis.title.y      = element_text(
+                            angle = 90,
+                            margin = margin(r = half_line / 2),
+                            vjust = 1
+                          ),
+      axis.title.y.right = element_text(
+                             angle = -90,
+                             margin = margin(l = half_line / 2),
+                             vjust = 0
+                           ),
+
+
       legend.background = element_blank(),
+      legend.spacing    = unit(font_size, "pt"),
+      legend.spacing.x  = NULL,
+      legend.spacing.y  = NULL,
+      legend.margin     = margin(0, 0, 0, 0),
       legend.key        = element_blank(),
       legend.key.size   = unit(1.1*font_size, "pt"),
-      legend.spacing    = unit(font_size, "pt"),
-      legend.box.spacing = unit(font_size, "pt"),
-      legend.margin     = margin(0, 0, 0, 0),
+      legend.key.height = NULL,
+      legend.key.width  = NULL,
       legend.text       = element_text(size = rel(rel_small)),
+      legend.text.align = NULL,
+      legend.title      = element_text(hjust = 0),
+      legend.title.align = NULL,
+      legend.position   = "right",
+      legend.direction  = NULL,
       legend.justification = c("left", "center"),
+      legend.box        = NULL,
+      legend.box.margin =  margin(0, 0, 0, 0),
+      legend.box.background = element_blank(),
+      legend.box.spacing = unit(font_size, "pt"),
+
       panel.background  = element_blank(),
       panel.border      = element_blank(),
-      panel.grid.major  = element_blank(),
-      panel.grid.minor  = element_blank(),
+      panel.grid        = element_blank(),
+      panel.grid.major  = NULL,
+      panel.grid.minor  = NULL,
       panel.spacing     = unit(half_line, "pt"),
-      strip.text        = element_text(size = rel(rel_small),
-                                       margin = margin(half_line, half_line, half_line, half_line)),
-      strip.background  = element_rect(fill = "grey80", colour = "grey50"),
+      panel.spacing.x   = NULL,
+      panel.spacing.y   = NULL,
+      panel.ontop       = FALSE,
+
+      strip.background  = element_rect(fill = "grey80"),
+      strip.text        = element_text(
+                            size = rel(rel_small),
+                            margin = margin(half_line / 2, half_line / 2,
+                                            half_line / 2, half_line / 2)
+                          ),
+      strip.text.x      = NULL,
+      strip.text.y      = element_text(angle = -90),
+      strip.placement   = "inside",
+      strip.placement.x =  NULL,
+      strip.placement.y =  NULL,
+      strip.switch.pad.grid = unit(half_line / 2, "pt"),
+      strip.switch.pad.wrap = unit(half_line / 2, "pt"),
+
       plot.background   = element_blank(),
-      plot.title        = element_text(face = "bold",
-                                       size = rel(rel_large),
-                                       hjust = 0, vjust = 1,
-                                       margin = margin(b = half_line)),
-      plot.subtitle     = element_text(size = rel(rel_small),
-                                       hjust = 0, vjust = 1,
-                                       margin = margin(b = half_line)),
-      plot.caption      = element_text(size = rel(rel_tiny),
-                                       hjust = 1, vjust = 1,
-                                       margin = margin(t = half_line)),
-      plot.margin =        margin(half_line, half_line, half_line, half_line),
+      plot.title        = element_text(
+                            face = "bold",
+                            size = rel(rel_large),
+                            hjust = 0, vjust = 1,
+                            margin = margin(b = half_line)
+                          ),
+      plot.subtitle     = element_text(
+                            size = rel(rel_small),
+                            hjust = 0, vjust = 1,
+                            margin = margin(b = half_line)
+                          ),
+      plot.caption      = element_text(
+                            size = rel(rel_tiny),
+                            hjust = 1, vjust = 1,
+                            margin = margin(t = half_line)
+                          ),
+      plot.margin       = margin(half_line, half_line, half_line, half_line),
 
       complete = TRUE
     )
