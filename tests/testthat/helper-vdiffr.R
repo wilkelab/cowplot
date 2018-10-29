e5e9d27ae106bc4c10960566d6f2b7fc57452452
Claus Wilke <wilke@austin.utexas.edu>
2018-10-29 00:26:36 -0500

    Remove patchwork dependence until it's available on CRAN; needed for CRAN release and also due to changes with github installs on Travis.

--- tests/testthat/helper-vdiffr.R
+++ tests/testthat/helper-vdiffr.R
@@ -1,4 +1,18 @@
-enable_vdiffr <- TRUE
+# default is equal to whether NOT_CRAN is true or not
+enable_vdiffr <- identical(Sys.getenv("NOT_CRAN"), "true")
+
+# disable or enable vdiffr based on the state of USE_VDIFFR, if set
+if (identical(Sys.getenv("USE_VDIFFR"), "true")) {
+  enable_vdiffr <- TRUE
+} else if (identical(Sys.getenv("USE_VDIFFR"), "false")) {
+  enable_vdiffr <- FALSE
+}
+
+# disable vdiffr if version is old
+if (!requireNamespace("vdiffr", quietly = TRUE) ||
+    utils::packageVersion("vdiffr") < "0.2.3.9001") {
+  enable_vdiffr <- FALSE
+}
 
 expect_doppelganger <- function(title, fig,
                                 path = NULL,
